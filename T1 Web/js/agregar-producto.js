72248bc4fe7f12e6977f3ec4bf9557e23b6593ff
ihumire <ignacio.humire@ug.uchile.cl>
2024-04-03 11:32:41 -0300

    Selección de productos terminada

--- T1 Web/js/agregar-producto.js
+++ T1 Web/js/agregar-producto.js
@@ -11,17 +11,26 @@ const productosTotales = [productos0, productosFrutas,productosVerduras];
 
 
 function setFrutaOrVerdura(value) {
-	let onSet = document.getElementById("producto1")
-	while(onSet.firstChild) {
-		onSet.removeChild(onSet.firstChild)
-	}
-	let conjProductosTotales = productosTotales[value];
-	let lenProductosTotales = conjProductosTotales.length;
-	for(i=0; i < lenProductosTotales; i+=1) {
-		let newOption = document.createElement("option")
-		newOption.value = conjProductosTotales[i];
-		newOption.text = conjProductosTotales[i];
-		onSet.appendChild(newOption)
+	let onSet1 = document.getElementById("producto1");
+	let onSet2 = document.getElementById("producto2");
+	let onSet3 = document.getElementById("producto3");
+	let onSet4 = document.getElementById("producto4");
+	let onSet5 = document.getElementById("producto5");
+	
+	const listOnSet = [onSet1, onSet2, onSet3, onSet4, onSet5];
+	const lenListOnSet = listOnSet.length;
+	for(k=0; k<lenListOnSet; k+=1){
+		while(listOnSet[k].firstChild) {
+			listOnSet[k].removeChild(listOnSet[k].firstChild)
+		}
+		let conjProductosTotales = productosTotales[value];
+		let lenProductosTotales = conjProductosTotales.length;
+		for(i=0; i < lenProductosTotales; i+=1) {
+			let newOption = document.createElement("option")
+			newOption.value = conjProductosTotales[i];
+			newOption.text = conjProductosTotales[i];
+			listOnSet[k].appendChild(newOption)
+		}
 	}
 }
 
@@ -30,8 +39,24 @@ function Validar() {
 	tipoProducto.required = true;
 	let ValidTipoProducto = tipoProducto.value != "0";
 	let producto1 = document.getElementById("producto1");
-	producto1.required = true;
-	let ValidProducto1 = producto1 != "--Seleccione una comuna--";
+	let ValidProducto1 = producto1.value != "--Seleccione un producto--";
+	let producto2 = document.getElementById("producto2");
+	let ValidProducto2 = producto2.value != "--Seleccione un producto--";
+	let producto3 = document.getElementById("producto3");
+	let ValidProducto3 = producto3.value != "--Seleccione un producto--";
+	let producto4 = document.getElementById("producto4");
+	let ValidProducto4 = producto4.value != "--Seleccione un producto--";
+	let producto5 = document.getElementById("producto5");
+	let ValidProducto5 = producto5.value != "--Seleccione un producto--";
+	
+	let ValidProductos = false;
+	let listValidProductos = [ValidProducto1, ValidProducto2, ValidProducto3, ValidProducto4, ValidProducto5];
+	let lenListValidProductos = listValidProductos.length;
+		for(i=0; i<lenListValidProductos; i+=1){
+		if (listValidProductos[i]){
+			ValidProductos = true;
+		}
+	}
 	
 	let space = document.getElementById("top");
 	let m1 = document.getElementById("E1");
@@ -46,15 +71,20 @@ function Validar() {
 		m1.style.display = "block";
 	} else {m1.style.display = "none";}
 
-	if (!ValidProducto1) {
+	if (!ValidProductos) {
 		m2.style.display = "block";
 	} else {m2.style.display = "none";}
 
-	if (ValidTipoProducto && ValidProducto1) {
+	if (ValidTipoProducto && ValidProductos) {
 			m9.style.display = "block";
 			botonConfirmar.style.display = "block";
 			botonNoConfirmar.style.display = "block";
 			tipoProducto.disabled = true;
+			producto1.disabled = true;
+			producto2.disabled = true;
+			producto3.disabled = true;
+			producto4.disabled = true;
+			producto5.disabled = true;
 		} 
 	else {
 		m9.style.display = "none";
@@ -67,8 +97,18 @@ function Validar() {
 function Felicitaciones() {
 	
 	let TipoProducto = document.getElementById("tipo-producto");
+	let producto1 = document.getElementById("producto1");
+	let producto2 = document.getElementById("producto2");
+	let producto3 = document.getElementById("producto3");
+	let producto4 = document.getElementById("producto4");
+	let producto5 = document.getElementById("producto5");
 	
 	TipoProducto.disabled = false;
+	producto1.disabled = false;
+	producto2.disabled = false;
+	producto3.disabled = false;
+	producto4.disabled = false;
+	producto5.disabled = false;
 	
 	alert("Agregación exitosa de productos.");
 }
@@ -76,6 +116,12 @@ function Felicitaciones() {
 function goBack() {
 	
 	let tipoProducto = document.getElementById("tipo-producto");
+	let producto1 = document.getElementById("producto1");
+	let producto2 = document.getElementById("producto2");
+	let producto3 = document.getElementById("producto3");
+	let producto4 = document.getElementById("producto4");
+	let producto5 = document.getElementById("producto5");
+	
 	let space = document.getElementById("top");
 	let m9 = document.getElementById("E9");
 	let botonConfirmar = document.getElementById("confirmar");
@@ -85,6 +131,11 @@ function goBack() {
 	m9.style.display = "none";
 	botonConfirmar.style.display = "none";
 	botonNoConfirmar.style.display = "none";
-	tipoProducto.disabled = false;
+	tipoProducto.disabled = false;	
+	producto1.disabled = false;
+	producto2.disabled = false;
+	producto3.disabled = false;
+	producto4.disabled = false;
+	producto5.disabled = false;
 	
 }
