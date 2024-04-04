6d471f1c5df176dd81d1b15281897d3c803139e5
ihumire <ignacio.humire@ug.uchile.cl>
2024-04-04 09:46:06 -0300

    Feat agregar-producto.html
    Add Region & Comuna in form

--- T1 Web/js/agregar-producto.js
+++ T1 Web/js/agregar-producto.js
@@ -9,6 +9,70 @@ const productosVerduras = ["--Seleccione un producto--", "Brócoli", "Repollo", 
 
 const productosTotales = [productos0, productosFrutas,productosVerduras];
 
+const comunas0 = ["--Seleccione una comuna--"];
+const comunas1 = ["--Seleccione una comuna--","Alto Hospicio","Camiña","Colchane","Huara",
+"Iquique","Pica","Pozo Almonte"];
+const comunas2 = ["--Seleccione una comuna--","Antofagasta","Calama","María Elena","Mejillones",
+"Ollagüe","San Pedro de Atacama","Sierra Gorda","Taltal","Tocopilla"];
+const comunas3 = ["--Seleccione una comuna--","Alto del Carmen","Caldera","Chañaral",
+"Copiapó","Diego de Almagro","Freirina","Huasco","Tierra Amarilla","Vallenar"];
+const comunas4 = ["--Seleccione una comuna--","Andacollo","Canela","Combarbalá","Coquimbo",
+"Illapel","La Higuera","La Serena","Los Vilos","Monte Patria","Ovalle","Paihuano","Punitaqui",
+"Río Hurtado","Salamanca","Vicuña"];
+const comunas5 = ["--Seleccione una comuna--","Algarrobo","Cabildo","Calle Larga","Cartagena",
+"Casablanca","Catemu","Concón","El Quisco","El Tabo","Hijuelas","Juan Fernández","La Calera",
+"La Cruz","La Ligua","Limache","Llay-Llay","Los Andes","Nogales","Olmué","Panquehue","Papudo",
+"Petorca","Puchuncaví","Putaendo","Quillota","Quilpué","Quintero","Rapa Nui","Rinconada",
+"San Antonio","San Esteban","San Felipe","Santa María","Santo Domingo","Valparaíso",
+"Villa Alemana","Viña del Mar","Zapallar"];
+const comunas6 = ["--Seleccione una comuna--","Chépica","Chimbarongo","Codegua","Coinco",
+"Coltauco","Doñihue","Graneros","La Estrella","Las Cabras","Litueche","Lolol","Machalí",
+"Malloa","Marchigüe","Mostazal","Nancagua","Navidad","Olivar","Palmilla","Paredones",
+"Peralillo","Peumo","Pichidegua","Pichilemu","Placilla","Pumanque","Quinta de Tilcoco",
+"Rancagua","Rengo","Requínoa","San Fernando","Santa Cruz","San Vicente de Tagua-Tagua"];
+const comunas7 = ["--Seleccione una comuna--","Cauquenes","Chanco","Colbún","Constitución",
+"Curepto","Curicó","Empedrado","Hualañé","Licantén","Linares","Longaví","Maule","Molina",
+"Parral","Pelarco","Pelluhue","Pencahue","Rauco","Retiro","Río Claro","Romeral",
+"Sagrada Familia","San Clemente","San Javier","San Rafael","Talca","Teno","Vichuquén",
+"Villa Alegre","Yerbas Buenas"];
+const comunas8 = ["--Seleccione una comuna--","Alto Biobío","Antuco","Arauco","Cabrero",
+"Cañete","Chiguayante","Concepción","Contulmo","Coronel","Curanilahue","Florida","Haulpén",
+"Hualqui","Laja","Lebu","Los Álamos","Los Ángeles","Lota","Mulchén","Nacimiento","Negrete",
+"Penco","Quilaco","Quilleco","San Pedro de la Paz","San Rosendo","Santa Bárbara",
+"Santa Juana","Talcahuano","Tirúa","Tomé","Tucapel","Yumbel"];
+const comunas9 = ["--Seleccione una comuna--","Angol","Carahue","Cholchol","Collipulli",
+"Cunco","Curacautín","Curarrehue","Ercilla","Freire","Galvarino","Gorbea","Lautaro",
+"Loncoche","Lonquimay","Los Sauces","Lumaco","Melipeuco","Nueva Imperial","Padre las Casas",
+"Perquenco","Pitrufquén","Pucón","Puerto Saavedra","Purén","Renaico","Temuco","Teodoro Schmidt",
+"Toltén","Traiguén","Victoria","Vilcún","Villarrica"];
+const comunas10 = ["--Seleccione una comuna--","Ancud","Calbuco","Castro","Chaitén","Chonchi",
+"Cochamó","Curaco de Vélez","Dalcahue","Fresia","Frutillar","Futaleufú","Hualaihué",
+"Llanquihue","Los Muermos","Maullín","Osorno","Palena","Puerto Montt","Puerto Octay",
+"Puerto Varas","Puqueldón","Purranque","Puyehue","Queilen","Quemchi","Quellón","Quinchao",
+"Río Negro","San Juan de la Costa","San Pablo"];
+const comunas11 = ["--Seleccione una comuna--","Aysén","Chile Chico","Cisnes","Cochrane",
+"Coyhaique","Guaitecas","Lago Verde","O'Higgins","Río Ibáñez","Tortel"];
+const comunas12 = ["--Seleccione una comuna--","Antártica","Cabo de Hornos","Laguna Blanca",
+"Natales","Porvenir","Primavera","Punta Arenas","Río Verde","San Gregorio","Timaukel",
+"Torres del Paine"];
+const comunas13 = ["--Seleccione una comuna--","Alhué","Buin","Calera de Tango","Cerrillos",
+"Cerro Navia","Colina","Conchalí","Curacaví","El Bosque","El Monte","Estación Central",
+"Huechuraba","Independencia","Isla de Maipo","La Cisterna","La Granja","La Florida","La Pintana",
+"La Reina","Lampa","Las Condes","Lo Barnechea","Lo Espejo","Lo Prado","Macul","Maipú",
+"María Pinto","Melipilla","Ñuñoa","Paine","Padre Hurtado","Pedro Aguirre Cerda","Peñaflor",
+"Peñalolén","Pirque","Providencia","Pudahuel","Puente Alto","Quilicura","Quinta Normal",
+"Recoleta","Renca","San Bernardo","San Miguel","San Joaquín","San José de Maipo","San Pedro",
+"San Ramón","Santiago","Talagante","Til Til","Vitacura"];
+const comunas14 = ["--Seleccione una comuna--","Corral","Futrono","Lago Ranco","Lanco",
+"La Unión","Los Lagos","Máfil","Mariquina","Paillaco","Panguipulli","Río Bueno","Valdivia"];
+const comunas15 = ["--Seleccione una comuna--","Arica","Camarones","General Lagos","Putre"];
+const comunas16 = ["--Seleccione una comuna--","Bulnes","Chillán","Chillán Viejo",
+"Cobquecura","Coelemu","Coihueco","El Carmen","Ninhue","Ñiquén","Pemuco","Pinto","Portezuelo",
+"Quillón","Quirihue","Ránquil","San Carlos","San Fabián","San Ignacio","San Nicolás",
+"Trehuaco","Yungay"];
+
+const allComunas = [comunas0, comunas1, comunas2, comunas3, comunas4, comunas5, comunas6, comunas7, comunas8, 
+comunas9, comunas10, comunas11, comunas12, comunas13, comunas14, comunas15, comunas16];
 
 function setFrutaOrVerdura(value) {
 	let onSet1 = document.getElementById("producto1");
@@ -21,16 +85,31 @@ function setFrutaOrVerdura(value) {
 	const lenListOnSet = listOnSet.length;
 	for(k=0; k<lenListOnSet; k+=1){
 		while(listOnSet[k].firstChild) {
-			listOnSet[k].removeChild(listOnSet[k].firstChild)
+			listOnSet[k].removeChild(listOnSet[k].firstChild);
 		}
 		let conjProductosTotales = productosTotales[value];
 		let lenProductosTotales = conjProductosTotales.length;
 		for(i=0; i < lenProductosTotales; i+=1) {
-			let newOption = document.createElement("option")
+			let newOption = document.createElement("option");
 			newOption.value = conjProductosTotales[i];
 			newOption.text = conjProductosTotales[i];
-			listOnSet[k].appendChild(newOption)
+			listOnSet[k].appendChild(newOption);
 		}
+	}
+}
+
+function setComuna(value) {
+	let onSet = document.getElementById("comuna");
+	while(onSet.firstChild) {
+		onSet.removeChild(onSet.firstChild);
+	}
+	let mostrarComunas = allComunas[value];
+	let lenMostrarComunas = mostrarComunas.length;
+	for(i=0; i < lenMostrarComunas; i+=1) {
+		let newOption = document.createElement("option");
+		newOption.value = mostrarComunas[i];
+		newOption.text = mostrarComunas[i];
+		onSet.appendChild(newOption);
 	}
 }
 
@@ -43,6 +122,9 @@ function Validar() {
 	let producto5 = document.getElementById("producto5");
 	let descripcion = document.getElementById("descripcion");
 	let fotos = document.getElementById("fotos");
+	let region = document.getElementById("region");
+	let comuna = document.getElementById("comuna");
+
 
 	tipoProducto.required = true;
 	let ValidTipoProducto = tipoProducto.value != "0";
@@ -62,11 +144,18 @@ function Validar() {
 	}
 
 	let ValidFotos = fotos.files.length > 0 && fotos.files.length < 4;
+	region.required = true;
+	let ValidRegion = region.value != "0";
+	comuna.required = true;
+	let ValidComuna = comuna.value != "--Seleccione una comuna--";
+
 	
 	let space = document.getElementById("top");
 	let m1 = document.getElementById("E1");
 	let m2 = document.getElementById("E2");
 	let m3 = document.getElementById("E3");
+	let m4 = document.getElementById("E4");
+	let m5 = document.getElementById("E5");
 	let m9 = document.getElementById("E9");
 	let botonConfirmar = document.getElementById("confirmar");
 	let botonNoConfirmar = document.getElementById("no-confirmar");
@@ -84,8 +173,16 @@ function Validar() {
 	if (!ValidFotos) {
 		m3.style.display = "block";
 	} else {m3.style.display = "none";}
+	
+	if (!ValidRegion) {
+		m4.style.display = "block";
+	} else {m4.style.display = "none";}
 
-	if (ValidTipoProducto && ValidProductos && ValidFotos) {
+	if (!ValidComuna) {
+		m5.style.display = "block";
+	} else {m5.style.display = "none";}
+
+	if (ValidTipoProducto && ValidProductos && ValidFotos && ValidRegion && ValidComuna) {
 		m9.style.display = "block";
 		botonConfirmar.style.display = "block";
 		botonNoConfirmar.style.display = "block";
@@ -97,7 +194,9 @@ function Validar() {
 		producto5.disabled = true;
 		descripcion.disabled = true;
 		fotos.disabled = true;
-		} 
+		region.disabled = true;
+		comuna.disabled = true;
+	} 
 	else {
 		m9.style.display = "none";
 		botonConfirmar.style.display = "none";
@@ -116,6 +215,8 @@ function Felicitaciones() {
 	let producto5 = document.getElementById("producto5");
 	let descripcion = document.getElementById("descripcion");
 	let fotos = document.getElementById("fotos");
+	let region = document.getElementById("region");
+	let comuna = document.getElementById("comuna");
 	
 	TipoProducto.disabled = false;
 	producto1.disabled = false;
@@ -125,6 +226,8 @@ function Felicitaciones() {
 	producto5.disabled = false;
 	descripcion.disabled = false;
 	fotos.disabled = false;
+	region.disabled = false;
+	comuna,disabled = false;
 	
 	alert("Agregación exitosa de productos.");
 }
@@ -139,6 +242,8 @@ function goBack() {
 	let producto5 = document.getElementById("producto5");
 	let descripcion = document.getElementById("descripcion");
 	let fotos = document.getElementById("fotos");
+	let region = document.getElementById("region");
+	let comuna = document.getElementById("comuna");
 
 	let space = document.getElementById("top");
 	let m9 = document.getElementById("E9");
@@ -157,5 +262,6 @@ function goBack() {
 	producto5.disabled = false;
 	descripcion.disabled = false;
 	fotos.disabled = false;
-
+	region.disabled = false;
+	comuna.disabled = false;
 }
